b3149f0d678fe72830f7bedad68e55f6cb0916f4
Connor Hood <connor_hood@yahoo.com>
2021-05-14 11:28:11 -0500

    added to serializers and began coding out detail view

--- music_library_project/music_lib_app/serializers.py
+++ music_library_project/music_lib_app/serializers.py
@@ -6,3 +6,14 @@ class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'release_date']
+
+    def create(self, validated_data):
+        return Song.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.artist = validated_data.get('artist', instance.artist)
+        instance.album = validated_data.get('album', instance.album)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.save()
+        return instance
--- music_library_project/music_lib_app/views.py
+++ music_library_project/music_lib_app/views.py
@@ -20,3 +20,12 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SongDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
