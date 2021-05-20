ebbb49a56e2de31382a28a309a0d3ded2ec84a3e
Connor Hood <connor_hood@yahoo.com>
2021-05-20 12:47:21 -0500

    added empty redirect view for main page api

--- music_library_project/music_lib_app/models.py
+++ music_library_project/music_lib_app/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
-    album = models.CharField(max_length=50)
+    album = models.CharField(max_length=250)
     release_date = models.DateTimeField()
--- music_library_project/music_lib_app/urls.py
+++ music_library_project/music_lib_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('music/', views.SongList.as_view()),
-    path('music/<int:pk>/', views.SongDetail.as_view())
+    path('music/<int:pk>/', views.SongDetail.as_view()),
+    path('', RedirectView.as_view(url='/music/')),
 ]
